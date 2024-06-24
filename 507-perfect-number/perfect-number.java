402e0caa8af8ef6ec62e62fa7fbf72c7a4bd5db8
Alfa24 <133508559+Sachan-aditya@users.noreply.github.com>
2024-06-24 12:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (8.45%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int s=0;
-        int nums=num;
-        for(int i=1;i<nums;i++)
-        {
-        if(nums%i==0)
-        s+=i;
-        }
-        if(s==num)
-        return true;
-        else
-        return false;
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
     }
 }
