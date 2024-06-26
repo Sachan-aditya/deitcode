1dcd61c278cedc9e0c186da95f1c586a24328d8c
Alfa24 <133508559+Sachan-aditya@users.noreply.github.com>
2024-06-27 00:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (52.60%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,18 +1,13 @@
-import java.util.StringTokenizer;
-
 class Solution {
     public int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (len > 0) break;
+            } else {
+                len++;
+            }
         }
-
-        StringTokenizer st = new StringTokenizer(s);
-        String lastWord = "";
-
-        while (st.hasMoreTokens()) {
-            lastWord = st.nextToken();
-        }
-
-        return lastWord.length();
+        return len;
     }
 }
