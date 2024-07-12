0fdfbf621b2290ac1bfe32d41925e3535f27b771
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-07-12 11:12:15 +0530

    Time: 20 ms (100.00%) | Memory: 45.2 MB (88.89%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        
+        int a = 0;
+        int b = 0;
+        int min = Math.min(x, y);
+        int res = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 'b') {
+                res += Math.min(a, b) * min;
+                a = 0;
+                b = 0;
+            } else if (c == 'a') {
+                if (x < y && b > 0) {
+                    b--;
+                    res += y;
+                } else {
+                    a++;
+                }
+            } else {
+                if (x > y && a > 0) {
+                    a--;
+                    res += x;
+                } else {
+                    b++;
+                }
+            }
+        }
+        
+        res += Math.min(a, b) * min;
+        
+        return res;
+    }
+}
