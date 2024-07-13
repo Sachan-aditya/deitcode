a96f9f47729808d896aecf164171e47a5e268503
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-07-13 16:48:11 +0530

    Time: 20 ms (100.00%) | Memory: 45.5 MB (76.19%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,54 +1,36 @@
 class Solution {
-    public int maximumGain(String s, int x, int y) 
-    {
-      String imp,unimp;
-      int imp_score,unimp_score;
-      if(x>y)
-      {
-        imp="ab";
-        imp_score=x;
-        unimp="ba";
-        unimp_score=y;
-      }
-      else
-      {
-        imp="ba";
-        imp_score=y;
-        unimp="ab";
-        unimp_score=x;
-      }
-      int score=0;
-      Stack<Character> stack=new Stack<>();
-        for(char ch:s.toCharArray())
-        {
-          if(ch==imp.charAt(1)&&stack.size()>0&&stack.peek()==imp.charAt(0))
-          {
-            score+=imp_score;
-            stack.pop();
-          }
-          else
-          {
-            stack.push(ch);
-          }
-        }
-        String str="";
-        while(!stack.isEmpty())
-        {
-          str=stack.pop()+str;
-        }
-        for(char ch:str.toCharArray())
-        {
-          if(ch==unimp.charAt(1)&&stack.size()>0&&stack.peek()==unimp.charAt(0))
-          {
-            score+=unimp_score;
-            stack.pop();
-          }
-          else
-          {
-            stack.push(ch);
-          }
-        }
-        return score;
+    public int maximumGain(String s, int x, int y) {
+        //cdbcbbaaabab
+        int a = 0;
+        int b = 0;
+        int min = Math.min(x, y);//4
+        int res = 0;
         
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 'b') {
+                res += Math.min(a, b) * min;//16
+                a = 0;
+                b = 0;
+            } else if (c == 'a') {
+                if (x < y && b > 0) {
+                    b--;
+                    res += y;
+                } else {
+                    a++;
+                }
+            } else {
+                if (x > y && a > 0) {
+                    a--;
+                    res += x;
+                } else {
+                    b++;
+                };
+            }
+        }
+        
+        res += Math.min(a, b) * min;
+        
+        return res;
     }
 }
