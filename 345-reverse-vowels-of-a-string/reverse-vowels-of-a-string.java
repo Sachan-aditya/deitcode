1916af2447d5c5fb6dcdbcb25477614788396ac3
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-07-14 22:27:07 +0530

    Time: 7 ms (26.59%) | Memory: 44.9 MB (45.39%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseVowels(String s) {
+      StringBuilder sb=new StringBuilder();
+      Stack<Character> stack=new Stack<>();
+     
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(c=='a'||c=='e'|| c=='o'||c=='u'|| c=='i'||c=='A'||c=='E'|| c=='O'||c=='U'|| c=='I')
+            {
+               stack.push(c);//eeoe
+            }  
+        }
+      
+          for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+             if(c=='a'||c=='e'|| c=='o'||c=='u'|| c=='i'||c=='A'||c=='E'|| c=='O'||c=='U'|| c=='I')
+            {
+               sb.append(stack.pop());
+            } 
+            else
+            sb.append(c) ;
+        }
+     
+        return sb.toString();
+    }
+}
