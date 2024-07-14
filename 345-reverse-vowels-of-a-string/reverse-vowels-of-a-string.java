220577139474177663280b8a47481194b0fdd4a2
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-07-14 22:31:03 +0530

    Time: 7 ms (26.59%) | Memory: 44.9 MB (45.39%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,27 +1,23 @@
 class Solution {
     public String reverseVowels(String s) {
-      StringBuilder sb=new StringBuilder();
-      Stack<Character> stack=new Stack<>();    
-        for(int i=0;i<s.length();i++)
-        {
-            char c=s.charAt(i);
-            if(c=='a'||c=='e'|| c=='o'||c=='u'|| c=='i'||c=='A'||c=='E'|| c=='O'||c=='U'|| c=='I')
-            {
-               stack.push(c);//eeoe
-            }  
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i' ||
+                c == 'A' || c == 'E' || c == 'O' || c == 'U' || c == 'I') {
+                stack.push(c);
+            }
         }
-      
-          for(int i=0;i<s.length();i++)
-        {
-            char c=s.charAt(i);
-             if(c=='a'||c=='e'|| c=='o'||c=='u'|| c=='i'||c=='A'||c=='E'|| c=='O'||c=='U'|| c=='I')
-            {
-               sb.append(stack.pop());
-            } 
-            else
-            sb.append(c) ;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i' ||
+                c == 'A' || c == 'E' || c == 'O' || c == 'U' || c == 'I') {
+                sb.append(stack.pop());
+            } else {
+                sb.append(c);
+            }
         }
-     
         return sb.toString();
     }
 }
