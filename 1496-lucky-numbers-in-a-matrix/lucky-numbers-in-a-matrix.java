8c314a694f34e8c8c2cdb26dee2186e1e0b088e4
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-07-19 10:53:40 +0530

    Time: 2 ms (93.93%) | Memory: 45.1 MB (51.10%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -2,34 +2,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> luckyNumbers(int[][] matrix) {
+    public List<Integer> luckyNumbers (int[][] matrix) {
         List<Integer> list = new ArrayList<>();
-        
+
         for (int i = 0; i < matrix.length; i++) {
             int min = Integer.MAX_VALUE;
-            int minCol = -1;
-
-            // Find the minimum value in the current row
+            int minIndex = -1;
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] < min) {
                     min = matrix[i][j];
-                    minCol = j;
+                    minIndex = j;
                 }
             }
 
-            boolean isMaxInCol = true;
+           
+            boolean isMaxInColumn = true;
             for (int k = 0; k < matrix.length; k++) {
-                if (matrix[k][minCol] > min) {
-                    isMaxInCol = false;
+                if (matrix[k][minIndex] > min) {
+                    isMaxInColumn = false;
                     break;
                 }
             }
 
-            if (isMaxInCol) {
+            if (isMaxInColumn) {
                 list.add(min);
             }
         }
-        
+
         return list;
     }
 }
+
+
