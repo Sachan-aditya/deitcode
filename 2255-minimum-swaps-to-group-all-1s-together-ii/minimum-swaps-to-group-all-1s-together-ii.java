7632dd5c43f0cfc38d7f2d5d8c2803dc3326e872
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-08-02 20:18:46 +0530

    Time: 13 ms (13.19%) | Memory: 59 MB (65.43%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,14 +1,6 @@
 class Solution {
     public int minSwaps(int[] nums) {
         int n = nums.length;
-        int[] a = new int[2 * n];
-        
-        // Create the extended array to simulate circular array
-        for (int i = 0; i < 2 * n; i++) {
-            a[i] = nums[i % n];
-        }
-        
-        // Count total number of 1s
         int totalone = 0;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 1) {
@@ -17,14 +9,13 @@ class Solution {
         }
 
         int i = 0, j = 0, csum = 0, max = 0;
-        
-        // Use sliding window to find the maximum number of 1s in a window of size totalone
+
         while (j < 2 * n) {
-            if (a[j] == 1) {
+            if (nums[j%n] == 1) {
                 csum++;
             }
             if (j - i + 1 > totalone) {
-                if (a[i] == 1) {
+                if (nums[i%n] == 1) {
                     csum--;
                 }
                 i++;
@@ -32,8 +23,7 @@ class Solution {
             max = Math.max(max, csum);
             j++;
         }
-        
-        // The result is the number of swaps needed
+
         return totalone - max;
     }
 }
