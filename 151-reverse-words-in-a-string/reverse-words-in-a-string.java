d130ea5ac3edaec73e4dec2ad319113525b30391
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-08-16 07:36:57 +0530

    Time: 5 ms (92.96%) | Memory: 43.2 MB (57.93%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,19 +1,22 @@
 class Solution {
     public String reverseWords(String s) {
       String ar[]=s.split(" ");
-      String res="";
+      //String res="";
+      StringBuilder res=new StringBuilder();
       for(int i=ar.length-1;i>=0;i--)
       {
         if(ar[i].length()==0)
         continue;
       if(res.length()==0)
       {
-        res+=ar[i];
+          res.append(ar[i]);
       }
       else
-      res+=" "+ar[i];
+      {
+        res.append(" ");
+      res.append(ar[i]);
 
     }
-
-    return res;
+      }
+    return res.toString();
 }}
