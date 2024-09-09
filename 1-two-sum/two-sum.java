975a08e6c851a477ddf6a8aa3567423e485775f0
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-09-09 18:41:12 +0530

    Time: 3 ms (67.42%) | Memory: 44.8 MB (48.34%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,12 +3,13 @@ class Solution {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-            int find=target-nums[i];
-            if(map.containsKey(find))
-            return new int []{map.get(find),i};
-            else
-            map.put(nums[i],i);
-        }
-        return new int[]{-1,-1};
+            int val=target-nums[i];
+            if(map.containsKey(val)) {
+             return new int[] {map.get(val),i};
+            }
+          map.put(nums[i],i);
+       
+          }
+          return new int[]{-1,-1};
     }
 }
