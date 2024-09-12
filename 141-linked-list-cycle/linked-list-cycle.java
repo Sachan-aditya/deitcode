f72bc54057b708f00d91035d173dba699e7798eb
SSID <133508559+Sachan-aditya@users.noreply.github.com>
2024-09-12 12:10:01 +0530

    Time: 5 ms (8.47%) | Memory: 45.2 MB (7.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -1,27 +1,42 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) {
+//  *         val = x;
+//  *         next = null;
+//  *     }
+//  * }
+//  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if(head==null)
+//         return false;
+//         ListNode slow=head;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null)
+//         {
+//             slow=slow.next;
+//             fast=fast.next.next;
+//             if(slow==fast)
+//             return true;
+//         }
+//         return false;
+//     }
+// }
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null)
-        return false;
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast)
-            return true;
+        Set<ListNode> set = new HashSet<>();
+        
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
         }
+        
         return false;
     }
 }
