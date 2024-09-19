dc4d4a8c2d5f2247b29c3e380bbf0897c548e8bf
SHARK24 <133508559+Sachan-aditya@users.noreply.github.com>
2024-09-20 00:49:58 +0530

    Time: 1 ms (46.26%) | Memory: 40.4 MB (94.81%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0)
+        return false;
+        int a[]={2,3,5};
+        for(int i:a)
+        {
+            while(n%i==0)
+            n/=i;
+        }
+        return n==1;
+    }
+}
