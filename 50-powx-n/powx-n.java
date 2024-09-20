64f7b59697a909cd3ef3538b9193f783e8344534
SHARK24 <133508559+Sachan-aditya@users.noreply.github.com>
2024-09-20 20:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (24.50%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        
-        return  Math.pow(x,n);
+    double res=1;
+    if(n<0)
+    {
+     x=1/x;
+     n=-n;
     }
+     while(n!=0)
+     {
+        if(n%2!=0)
+        res=res*x;
+     
+     n/=2;
+     x*=x;
+    }
+    return res;
+}
 }
