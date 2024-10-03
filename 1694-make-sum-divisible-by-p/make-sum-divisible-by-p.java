73bdddda467a65aacbeab6f0f9e88d60cf51689b
SHARK24 <133508559+Sachan-aditya@users.noreply.github.com>
2024-10-03 12:25:42 +0530

    Time: 24 ms (81.69%) | Memory: 56 MB (91.55%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,29 +1,30 @@
-import java.util.HashMap;
-
 class Solution {
     public int minSubarray(int[] nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+    long s = 0;
+        for (int i : nums) {
+            s += i;
         }
-        int rem = (int)(totalSum % p);
-        if (rem == 0) return 0;
-        HashMap<Integer, Integer> prefixMod = new HashMap<>();
-        prefixMod.put(0, -1); 
-        long prefixSum = 0;
-        int minLength = nums.length;
-        for (int i = 0; i < nums.length; ++i) {
-            prefixSum += nums[i];
-            int currentMod = (int)(prefixSum % p);
-            int targetMod = (currentMod - rem + p) % p;
-
-            if (prefixMod.containsKey(targetMod)) {
-                minLength = Math.min(minLength, i - prefixMod.get(targetMod));
+        int rem=(int)(s%p);
+        if (rem == 0) {
+            return 0;
+        } else {
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, -1);
+            long sum = 0;
+            int minLen = nums.length;
+            for (int i = 0; i < nums.length; i++) {
+                sum+=nums[i];
+                int c=(int)(sum%p);
+                int target = (c- rem+ p) % p;
+                if (map.containsKey(target)) {
+                    minLen = Math.min(minLen, i - map.get(target));
+                }
+                map.put(c, i);
             }
-
-            prefixMod.put(currentMod, i);
+            if(minLen==nums.length)
+            return -1;
+            else
+           return minLen;
         }
-
-        return minLength == nums.length ? -1 : minLength;
     }
 }
