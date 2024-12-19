ad3336cc40b968f8ab50893e16d32560051ae849
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-19 19:41:11 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (66.01%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,22 +10,23 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy =new ListNode();
-        ListNode ans=dummy;
-        int carry=0;
-        while(l1!=null || l2 !=null || carry!=0)
+        ListNode dummy=new ListNode(0);//243 // 564
+        ListNode curr=dummy;
+        int c=0;
+        while(l1!=null || l2!=null || c>0)
         {
-            int x=(l1!=null)?l1.val:0;
-            int y=(l2!=null)?l2.val:0;
-            int sum=x+y+carry;
-            carry=sum/10;
-            ans.next=new ListNode(sum%10);
-            ans=ans.next;
-            if(l1!=null)
-            l1=l1.next;
-            if(l2!=null)
-            l2=l2.next;
+           int x=(l1!=null)?l1.val:0;//2
+           int y=(l2!=null)?l2.val:0;//5
+           int s=x+y+c;//7
+           c=s/10;//0
+           curr.next=new ListNode(s%10);//7
+           curr=curr.next;
+          if(l1!=null) l1=l1.next;
+          if(l2!=null) l2=l2.next;
+       
         }
-        return dummy.next;
-    }
+         return dummy.next;
+
+        }
+    
 }
