8c71ce8ffee8d904ae6c418eb0415b499911c2b3
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-24 17:18:45 +0530

    Time: 15 ms (25.81%) | Memory: 45.3 MB (30.22%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(char c:magazine.toCharArray())
+    public boolean canConstruct(String r, String m) {
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(char c:m.toCharArray())
         {
             map.put(c,map.getOrDefault(c,0)+1);
         }
-        for(char c:ransomNote.toCharArray())
+        for(char c:r.toCharArray())
         {
             if(map.containsKey(c) && map.get(c)>0)
             map.put(c,map.get(c)-1);
-            else
-            return false;
+        else
+        return false;
         }
         return true;
     }
