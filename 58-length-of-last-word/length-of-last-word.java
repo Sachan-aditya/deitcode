51a043592fda46467d077057622ea80b01cfe19d
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-25 22:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (48.89%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -2,10 +2,9 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int r=s.length()-1;int c=0;
         while(r>=0 && s.charAt(r)==' ')
+        r--;
+        while(r>=0 && s.charAt(r)!=' ')
         {
-            r--;
-        }
-        while(r>=0 && s.charAt(r)!=' '){
             c++;
             r--;
         }
