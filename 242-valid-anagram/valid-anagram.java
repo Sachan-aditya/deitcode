7340c63b0b6f4fcc8a4cdcea2b045fc0e24256eb
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-25 23:47:23 +0530

    Time: 16 ms (25.18%) | Memory: 45.4 MB (5.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,26 @@
 class Solution {
-    public boolean isAnagram(String s, String p) {
-     int m1[]=new int[26];
-     int m2[]=new int[26];
-     if (s.length() != p.length()) return false;
-     for(int i=0;i<p.length();i++)
-     {
-        m1[p.charAt(i)-'a']++;
-     }for(int i=0;i<s.length();i++)
-     {
-        m2[s.charAt(i)-'a']++;
-            if (i >= p.length()) {
-                m2[s.charAt(i - p.length()) - 'a']--;
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int e=0;
+         if (s.length() != t.length()) {
+            return false;
+        }
+        for(char x:t.toCharArray())
+        {
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+        for(char p:s.toCharArray())
+        {
+            if(map.containsKey(p)){
+            map.put(p,map.get(p)-1);
+            if(map.get(p)==0)
+            map.remove(p);
             }
+            else
+            return false;
+        
+        }
+       return map.isEmpty();
+    }
 
-            if (i >= p.length() - 1 && isMatched(m1, m2)) {
-               return true;
-            }
-        }
-        return false;
-    }
-    private boolean isMatched(int[] map1, int[] map2) {
-        for (int i = 0; i < 26; i++) {
-            if (map1[i] != map2[i]) 
-                return false;
-        }
-        return true;
-    }
 }
-     
