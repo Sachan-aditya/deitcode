5b5074a7a34ff288e29184c5e627419617e5da02
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-26 23:49:03 +0530

    Time: 6 ms (92.89%) | Memory: 42.8 MB (18.76%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int count[]=new int[256];
-        int count2[]=new int[256];
+        int map[]=new int[256];//egg
+        int map2[]=new int[256];//ade
         for(int i=0;i<s.length();i++)
         {
-            char c1=s.charAt(i);
-            char c2=t.charAt(i);
-            if(count[c1]!=count2[c2])
+            char c1=s.charAt(i);//e//g//g
+            char c2=t.charAt(i);//a//d//e
+            if(map[c1]!=map2[c2])//1!=2
             return false;
-            count[c1]=i+1;
-            count2[c2]=i+1;
+            map[c1]=i+1;//map[e]=1//map[g]=2
+            map2[c2]=i+1;//map[a]=1//map[d]=2
         }
         return true;
     }
