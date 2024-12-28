4ea9e880e495ca0ddd8d05936baa820bd34dff46
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2024-12-28 19:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (66.42%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -6,7 +6,7 @@ class Solution {
             while(strs[i].indexOf(prefix)!=0)
             {
                 prefix=prefix.substring(0,prefix.length()-1);
-                if(prefix.isEmpty())
+                if(prefix.length()==0)
                 return "";
             }
         }
