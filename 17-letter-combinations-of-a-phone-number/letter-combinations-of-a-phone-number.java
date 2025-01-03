aba15644580253be62420edb14b208c4301a77c3
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2025-01-03 17:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (20.37%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,33 +2,35 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         Map<Character,String> map=new HashMap<>();
         map.put('2',"abc");
-         map.put('3',"def");
-          map.put('4',"ghi");
-           map.put('5',"jkl"); 
-            map.put('6',"mno");
-           map.put('7',"pqrs");
-            map.put('8',"tuv");
-            map.put('9',"wxyz");
-            List<String> res=new ArrayList<>();
-            if(digits.isEmpty())
-            return res;
-            backtrack(digits,0,new StringBuilder(),res,map);
-            return res;
-    }
-    private void backtrack(String digits,int index, StringBuilder current,List<String>res,Map<Character,String>map)
-    {
-        if(index==digits.length()){
-        res.add(current.toString());
-        return;
-        }
-        char c=digits.charAt(index);
-        String l=map.get(c);
-        for(char ch:l.toCharArray())
-        {
-            current.append(ch);
-            backtrack(digits,index+1,current,res,map);
-            current.deleteCharAt(current.length()-1);
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        List<String> list=new ArrayList<>();
+        if(digits.length()==0)
+        return list;
+        backtrack(digits,0,new StringBuilder(),map,list);
+        return list;
 
-        }
     }
+    private void backtrack(String digits,int index,StringBuilder sb,Map<Character,String> map,List<String> list)
+{
+    if(index==digits.length())
+    {
+        list.add(sb.toString());
+        return;
+    }
+    char c=digits.charAt(index);
+    String letter=map.get(c);
+    for(char ch:letter.toCharArray())
+    {
+        sb.append(ch);
+        backtrack(digits,index+1,sb,map,list);
+    sb.deleteCharAt(sb.length()-1);
+        
+    }
+}
 }
