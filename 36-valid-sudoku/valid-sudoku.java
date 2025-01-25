a8efc4934209d04bbd4802e9ea4661f3ac9a95c0
adityasachan <133508559+Sachan-aditya@users.noreply.github.com>
2025-01-25 17:59:25 +0530

    Time: 14 ms (18.26%) | Memory: 44.8 MB (34.04%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -5,16 +5,15 @@ class Solution {
         {
             for(int j=0;j<9;j++)
             {
-                char current=board[i][j];
-                if(current !='.')
-                {
-                    String row=current+"row"+i;
-                    String column=current+"column"+j;
-                    String box=current+"box"+(i/3)+"-"+(j/3);
-                
-                if(!set.add(row)||!set.add(column)||!set.add(box))
+                char c=board[i][j];
+                if(c!='.'){
+                String p=c+"row"+i;
+                String q=c+"column"+j;
+                String r=c+"box"+(i/3)+"-"+(j/3);
+                if(!set.add(p)||!set.add(q)|| !set.add(r))
                 return false;
-                   }   }
+            }
+            }
         }
         return true;
     }
